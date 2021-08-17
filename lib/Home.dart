15e74428674db787350ac416e5127b55dd296cf9
José Venancio Mosquera <venancio.mosquera@gmail.com>
2021-08-17 13:35:58 -0300

    Aula 156 - Firebase Udemy - Selec. Img. Cam/Galer

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,10 +12,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ImagePicker _picker = ImagePicker();
+
+  XFile? _image;
+
+  Future recoverImage(bool fromCamera) async {
+    XFile? selectedImage;
+    if (fromCamera) {
+      selectedImage = await _picker.pickImage(source: ImageSource.camera);
+    } else {
+      selectedImage = await _picker.pickImage(source: ImageSource.gallery);
+    }
+
+    setState(() {
+      _image = selectedImage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Title(
+          color: Colors.white,
+          child: Text("Selecionar imagem"),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Upload não iniciado"),
+              ElevatedButton(
+                onPressed: () {
+                  recoverImage(true);
+                },
+                child: Text("Câmera"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recoverImage(false);
+                },
+                child: Text("Galeria"),
+              ),
+              _image == null ? Container() : Image.file(File(_image!.path)),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Upload Storage"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
